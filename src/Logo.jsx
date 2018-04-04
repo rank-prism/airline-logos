e21d272ba255d778093a11b4d3d9c6f2acf94c08
Mark Prins <mark.prins@serko.com>
2018-04-05 09:22:49 +1200

    no-op to test deployment

--- src/Logo.jsx
+++ src/Logo.jsx
@@ -2,15 +2,15 @@ import * as React from 'react';
 import { TableCell, TableRow } from 'material-ui/Table';
 const base_url = 'https://serkowebtest.blob.core.windows.net/airline-logos';
 class Logo extends React.Component {
-render() {
-    return (
-        <TableRow>
-            <TableCell><strong>{this.props.code}</strong>  : {this.props.name}</TableCell>
-            <TableCell><img alt={this.props.code} src={`${base_url}/${this.props.code}_1x.png`} /></TableCell>
-            <TableCell><img alt={this.props.code} src={`${base_url}/${this.props.code}_2x.png`} /> </TableCell>
-            <TableCell><img alt={this.props.code} src={`${base_url}/${this.props.code}_4x.png`} /> </TableCell>
-        </TableRow>
-        );}
+    render() {
+        return (
+            <TableRow>
+                <TableCell><strong>{this.props.code}</strong>  : {this.props.name}</TableCell>
+                <TableCell><img alt={this.props.code} src={`${base_url}/${this.props.code}_1x.png`} /></TableCell>
+                <TableCell><img alt={this.props.code} src={`${base_url}/${this.props.code}_2x.png`} /> </TableCell>
+                <TableCell><img alt={this.props.code} src={`${base_url}/${this.props.code}_4x.png`} /> </TableCell>
+            </TableRow>
+            );}
 }
 
 export default Logo;
