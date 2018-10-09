0983862475ab447d9989279de5446130b5a38d5c
Mark Prins <mark@malcolmprins.net>
2018-10-10 09:55:41 +1300

    package version updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
 import Logo from './Logo';
-import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import airlines from './Airlines';
 
 class App extends Component {
@@ -12,7 +16,7 @@ class App extends Component {
       failures: 0,
       solUrlTemp: url,
       solUrl: url,
-      tempSolUrlValid: this.isValid(url)
+      tempSolUrlValid: this.isValid(url),
     };
 
     this.handleError = this.handleError.bind(this);
@@ -22,58 +26,74 @@ class App extends Component {
   }
 
   handleError = () => {
-    this.setState((prev, props) => ({failures: prev.failures+1}))
-  }
+    this.setState((prev, props) => ({ failures: prev.failures + 1 }));
+  };
 
-  handleSolUrlChange = (event) => {
+  handleSolUrlChange = event => {
     const value = event.target.value;
     const isValid = this.isValid(value);
-    this.setState((prev, props) => ({solUrlTemp: value, tempSolUrlValid: isValid}))
-  }
+    this.setState((prev, props) => ({ solUrlTemp: value, tempSolUrlValid: isValid }));
+  };
 
-  isValid = (url) => {
-    const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
+  isValid = url => {
+    const regex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/;
     return regex.test(url);
-  }
+  };
 
   updateSolUrl = () => {
-    if(this.state.tempSolUrlValid) {
-      this.setState((prev, props) => ({solUrl: prev.solUrlTemp}));
+    if (this.state.tempSolUrlValid) {
+      this.setState((prev, props) => ({ solUrl: prev.solUrlTemp }));
     }
-  }
+  };
 
-  handleSubmit = (event) => {
+  handleSubmit = event => {
     event.preventDefault();
-  }
+  };
 
   render() {
-    
     return (
       <div className="App">
-        <h1>
-          Airline Logos
-        </h1>
-        {this.state.failures > 0 && <div style={{color: 'red'}}>failures: {this.state.failures}</div>}
+        <h1>Airline Logos</h1>
+        {this.state.failures > 0 && (
+          <div style={{ color: 'red' }}>failures: {this.state.failures}</div>
+        )}
         <div>Total logos: {airlines.length}</div>
         <div>
           <form onSubmit={this.handleSubmit}>
-          <span style={this.state.tempSolUrlValid ? {} : {color: 'red'}}>Serko Online Url: </span><input type="text" size="50" onChange={this.handleSolUrlChange} onBlur={this.updateSolUrl} value={this.state.solUrlTemp}/>
-          {this.state.tempSolUrlValid}
+            <span style={this.state.tempSolUrlValid ? {} : { color: 'red' }}>
+              Serko Online Url:{' '}
+            </span>
+            <input
+              type="text"
+              size="50"
+              onChange={this.handleSolUrlChange}
+              onBlur={this.updateSolUrl}
+              value={this.state.solUrlTemp}
+            />
+            {this.state.tempSolUrlValid}
           </form>
         </div>
         <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Airline</TableCell>
-            {this.state.solUrl && <TableCell>Classic</TableCell>}
-            <TableCell>1x</TableCell>
-            <TableCell>2x</TableCell>
-            <TableCell>4x</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-        {airlines.map((airline, n) => <Logo code={airline[0]} key={n} name={airline[1]} onError={this.handleError} solUrl={this.state.solUrl}/>)}
-        </TableBody>
+          <TableHead>
+            <TableRow>
+              <TableCell>Airline</TableCell>
+              {this.state.solUrl && <TableCell>Classic</TableCell>}
+              <TableCell>1x</TableCell>
+              <TableCell>2x</TableCell>
+              <TableCell>4x</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {airlines.map((airline, n) => (
+              <Logo
+                code={airline[0]}
+                key={n}
+                name={airline[1]}
+                onError={this.handleError}
+                solUrl={this.state.solUrl}
+              />
+            ))}
+          </TableBody>
         </Table>
       </div>
     );
--- src/Logo.jsx
+++ src/Logo.jsx
@@ -1,25 +1,44 @@
 import * as React from 'react';
-import { TableCell, TableRow } from 'material-ui/Table';
+import TableCell from '@material-ui/core/TableCell';
+import TableRow from '@material-ui/core/TableRow';
 const base_url = 'https://serkowebtest.blob.core.windows.net/airline-logos';
 class Logo extends React.Component {
-    state = {error: false}
-    handleError = (e) => {
-        if(this.props.onError) this.props.onError(e);
-        this.setState(() => ({error: true}));
-    }
-    render() {
-        const sol_url = this.props.solUrl;
-        return (
-            <TableRow style={this.state.error ? {backgroundColor: '#e86f6f'} : {}}>
-                <TableCell><strong>{this.props.code}</strong>  : {this.props.name}</TableCell>
-                {sol_url && 
-                    <TableCell><img alt={this.props.code} src={`${sol_url}/SOLV2/Images/SupplierLogos/airlines/${this.props.code}.gif`} /></TableCell>
-                }
-                <TableCell><img alt={this.props.code} src={`${base_url}/${this.props.code}_1x.png`} onError={this.handleError}/></TableCell>
-                <TableCell><img alt={this.props.code} src={`${base_url}/${this.props.code}_2x.png`} /> </TableCell>
-                <TableCell><img alt={this.props.code} src={`${base_url}/${this.props.code}_4x.png`} /> </TableCell>
-            </TableRow>
-            );}
+  state = { error: false };
+  handleError = e => {
+    if (this.props.onError) this.props.onError(e);
+    this.setState(() => ({ error: true }));
+  };
+  render() {
+    const sol_url = this.props.solUrl;
+    return (
+      <TableRow style={this.state.error ? { backgroundColor: '#e86f6f' } : {}}>
+        <TableCell>
+          <strong>{this.props.code}</strong> : {this.props.name}
+        </TableCell>
+        {sol_url && (
+          <TableCell>
+            <img
+              alt={this.props.code}
+              src={`${sol_url}/SOLV2/Images/SupplierLogos/airlines/${this.props.code}.gif`}
+            />
+          </TableCell>
+        )}
+        <TableCell>
+          <img
+            alt={this.props.code}
+            src={`${base_url}/${this.props.code}_1x.png`}
+            onError={this.handleError}
+          />
+        </TableCell>
+        <TableCell>
+          <img alt={this.props.code} src={`${base_url}/${this.props.code}_2x.png`} />{' '}
+        </TableCell>
+        <TableCell>
+          <img alt={this.props.code} src={`${base_url}/${this.props.code}_4x.png`} />{' '}
+        </TableCell>
+      </TableRow>
+    );
+  }
 }
 
 export default Logo;
